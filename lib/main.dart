7be65319dbf11719741070a6aa1cf5b538339a82
SHINYA <ttiuweqpp2901@gmail.com>
2022-08-26 08:52:19 +0900

    [delete]TESTファイル作成につきルート画面遷移をAppファイルに移動

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:wordstock/pages/folder_page/folder_edit_page.dart';
-import 'package:wordstock/pages/folder_page/folder_page.dart';
-import 'package:wordstock/pages/play_page/play_page.dart';
-import 'package:wordstock/pages/play_page/play_result_page.dart';
-import 'package:wordstock/pages/word_page/word_edit_page.dart';
-import 'package:wordstock/pages/word_page/word_page.dart';
-import 'package:wordstock/pages/word_page/word_registration_page.dart';
+import 'package:wordstock/app.dart';
 
-main() {
-  runApp(
-    ProviderScope(
-      child: ScreenUtilInit(
-          designSize: const Size(431, 732),
-          minTextAdapt: true,
-          splitScreenMode: true,
-          builder: (context, _) {
-            return MaterialApp(
-              initialRoute: '/',
-              routes: <String, WidgetBuilder>{
-                '/': (BuildContext context) => const FolderPage(),
-                '/folder_edit_page': (BuildContext context) =>
-                    const FolderEditPage(),
-                '/word_page': (BuildContext context) => const WordPage(),
-                '/word_registration_page': (BuildContext context) =>
-                    const WordRegistrationPage(),
-                '/word_edit_page': (BuildContext context) =>
-                    const WordEditPage(),
-                '/play_page': (BuildContext context) => const PlayPage(),
-                '/play_result_page': (BuildContext context) =>
-                    const PlayResultPage(),
-              },
-            );
-          }),
-    ),
-  );
-}
+main() => runApp(const ProviderScope(child: const App()));
