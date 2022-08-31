8b53716e5ccf7302d84b63b15096594639964806
SHINYA <ttiuweqpp2901@gmail.com>
2022-09-01 00:20:34 +0900

    [fix]swipeCardsProviderを削除しPlayPageController管理

--- lib/pages/play_page/play_page_controller.dart
+++ lib/pages/play_page/play_page_controller.dart
@@ -1,81 +1,60 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:swipe_cards/swipe_cards.dart';
 import 'package:wordstock/model/word/word.dart';
-import 'package:wordstock/pages/play_page/swipe_cards_controller.dart';
 import 'package:wordstock/repository/sqlite_repository.dart';
 
-final allWordsProvider = FutureProvider.autoDispose(
-    (ref) => ref.read(sqliteRepositoryProvider).getWords());
+final pointWordsProvider = FutureProvider.autoDispose
+    .family<List<Word>, String>((ref, folderId) =>
+        ref.read(sqliteRepositoryProvider).getPointWords(folderId));
 
-final playsProvider = StateNotifierProvider.autoDispose<PlayPageController,
-    AsyncValue<List<Word>>>((ref) {
+final playsProvider = StateNotifierProvider.autoDispose
+    .family<PlayPageController, List<SwipeItem>, List<Word>>((ref, words) {
   final sqliteRepo = ref.read(sqliteRepositoryProvider);
-  final allWords = ref.watch(allWordsProvider);
-  //TODO:コメントアウトは資料の シート「自己対応」の箇所
-  // final MatchEngine matchEngine;
-  // allWords.value == null
-  //     ? matchEngine = MatchEngine(swipeItems: [])
-  //     : matchEngine = ref.watch(swipeCardsProvider(allWords));
-  final matchEngine = ref.watch(swipeCardsProvider(allWords));
-  return PlayPageController(sqliteRepo, allWords, matchEngine);
+
+  final swipeItems = List.generate(
+    words.length,
+    (index) => SwipeItem(
+      content: words,
+    ),
+  );
+
+  final matchEngine = MatchEngine(swipeItems: swipeItems);
+  final pointWordProvider =
+      ref.watch(pointWordsProvider(words[0].folderNameId!));
+
+  return PlayPageController(
+      swipeItems, matchEngine, sqliteRepo, pointWordProvider);
 });
 
-class PlayPageController extends StateNotifier<AsyncValue<List<Word>>> {
-  PlayPageController(this.sqliteRepo, this.allWords, this.matchEngine)
-      : super(allWords);
+class PlayPageController extends StateNotifier<List<SwipeItem>> {
+  PlayPageController(
+      List<SwipeItem> swipeItems, this.matchEngine, this.sqliteRepo, this.words)
+      : super(swipeItems);
 
   final SqliteRepository sqliteRepo;
-  final AsyncValue<List<Word>> allWords;
+
   final MatchEngine matchEngine;
 
-  get nope => matchEngine.currentItem?.nope();
+  final AsyncValue<List<Word>> words;
 
-  void superLike() => matchEngine.currentItem?.superLike();
-
-  void like() => matchEngine.currentItem?.like();
-
-  // MatchEngine? get matchEngine => state.value != null
-  //     ? MatchEngine(
-  //         swipeItems:
-  //             state.value!.map((e) => SwipeItem(content: e.id)).toList())
-  //     : null;
-
-  Future<void> playBadUp(String upId) async {
-    for (int i = 0; i < state.value!.length; i++) {
-      if (state.value![i].id == upId) {
-        state.value![i] =
-            state.value![i].copyWith(noCount: state.value![i].noCount! + 1);
-        state.value![i] =
-            state.value![i].copyWith(play: state.value![i].play! + 1);
-        state.value![i] = state.value![i].copyWith(ok: 'NG');
-        await sqliteRepo.upWord(state.value![i]);
-        state = AsyncValue.data([...state.value!]);
+  //get nope => matchEngine.currentItem!.nope();
+  void nope() {
+    matchEngine.currentItem?.nope();
+    for (int i = 0; i < state.length; i++) {
+      if (matchEngine.currentItem?.hashCode == state[i].hashCode) {
+        state[i].content[i] = state[i].content[i].copyWith(ok: 'NG');
+        sqliteRepo.upWord(state[i].content[i]);
       }
     }
   }
 
-  Future<void> playFlat(String folderId) async {
-    final flatValue = state.value!.map((flatValue) {
-      if (flatValue.folderNameId == folderId) {
-        flatValue = flatValue.copyWith(ok: 'FLAT');
-        sqliteRepo.upWord(flatValue);
-        return flatValue;
-      } else {
-        return flatValue;
+  void like() {
+    matchEngine.currentItem?.like();
+    for (int i = 0; i < state.length; i++) {
+      if (matchEngine.currentItem?.hashCode == state[i].hashCode) {
+        state[i].content[i] = state[i].content[i].copyWith(ok: 'OK');
+        sqliteRepo.upWord(state[i].content[i]);
       }
-    }).toList();
-    state = AsyncValue.data([...flatValue]);
-  }
-
-  Future<void> playGoodUp(String wordId) async {
-    state.value!.map((stateValue) async {
-      if (stateValue.id == wordId) {
-        stateValue = stateValue.copyWith(yesCount: stateValue.yesCount! + 1);
-        stateValue = stateValue.copyWith(play: stateValue.play! + 1);
-        stateValue = stateValue.copyWith(ok: 'OK');
-        await sqliteRepo.upWord(stateValue);
-        state = AsyncValue.data([...state.value!]);
-      }
-    });
+    }
   }
 }
