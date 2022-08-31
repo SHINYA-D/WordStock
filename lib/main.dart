4b225e4d4eb98b01ce3c5aff707c41f4b240ec52
SHINYA <ttiuweqpp2901@gmail.com>
2022-09-01 00:14:38 +0900

    [fix]constを修正削除

--- lib/main.dart
+++ lib/main.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wordstock/app.dart';
 
-main() => runApp(const ProviderScope(child: const App()));
+main() => runApp(const ProviderScope(child: App()));
