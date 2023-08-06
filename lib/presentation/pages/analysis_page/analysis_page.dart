26adeb7d8e4c6e4563382e4f43c36f79233412ae
SHINYA <ttiuweqpp2901@gmail.com>
2023-08-06 19:02:23 +0900

    グラデーション

--- lib/presentation/pages/analysis_page/analysis_page.dart
+++ lib/presentation/pages/analysis_page/analysis_page.dart
@@ -14,75 +14,88 @@ class AnalysisPage extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final folders = ref.watch(analysisProvider);
 
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text('分析画面'),
-        automaticallyImplyLeading: true,
-      ),
-      body: SlidableAutoCloseBehavior(
-        child: folders.value!.isEmpty
-            ? const Center(
-                child: Text(
-                'フォルダ登録がされておりません',
-                style: TextStyle(color: Colors.white),
-              ),)
-            : folders.when(
-                error: (error, stackTrace) =>
-                    Text('エラーが発生しました。\n ${error.toString()}'),
-                loading: () => const CircularProgressIndicator(),
-                data: (folder) => Padding(
-                  padding: EdgeInsets.only(top: 30.h),
-                  child: CarouselSlider(
-                    options: CarouselOptions(height: 500.0.h),
-                    items: folder!.map((folder) {
-                      return Builder(
-                        builder: (BuildContext context) {
-                          return Column(children: [
-                            Container(
-                              height: 150.h,
-                              width: MediaQuery.of(context).size.width,
-                              margin:
-                                  const EdgeInsets.symmetric(horizontal: 5.0),
-                              decoration:BoxDecoration(
-                                color: Colors.blue,
-                                borderRadius: BorderRadius.circular(10),
-                              ),
-                              child: Align(
-                                alignment: Alignment.center, // テキストを中央に配置
-                                child: Text(
-                                  folder.name ?? '表示中にエラーが発生しました',
-                                  style: const TextStyle(
-                                    fontSize: 32.0,
-                                    color: Colors.white,
+    return Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.pink,
+              Colors.blue,
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          appBar: AppBar(
+            centerTitle: true,
+            title: const Text('分析画面'),
+            automaticallyImplyLeading: true,
+          ),
+          body: SlidableAutoCloseBehavior(
+            child: folders.value!.isEmpty
+                ? const Center(
+                    child: Text(
+                      'フォルダ登録がされておりません',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                : folders.when(
+                    error: (error, stackTrace) =>
+                        Text('エラーが発生しました。\n ${error.toString()}'),
+                    loading: () => const CircularProgressIndicator(),
+                    data: (folder) => Padding(
+                      padding: EdgeInsets.only(top: 30.h),
+                      child: CarouselSlider(
+                        options: CarouselOptions(height: 500.0.h),
+                        items: folder!.map((folder) {
+                          return Builder(
+                            builder: (BuildContext context) {
+                              return Column(children: [
+                                Container(
+                                  height: 150.h,
+                                  width: MediaQuery.of(context).size.width,
+                                  margin: const EdgeInsets.symmetric(
+                                      horizontal: 5.0),
+                                  decoration: BoxDecoration(
+                                    color: Colors.blue,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Align(
+                                    alignment: Alignment.center, // テキストを中央に配置
+                                    child: Text(
+                                      folder.name ?? '表示中にエラーが発生しました',
+                                      style: const TextStyle(
+                                        fontSize: 32.0,
+                                        color: Colors.white,
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ),
-                            const Gap(100),
-                            CircularPercentIndicator(
-                              animation: true,
-                              animationDuration: 3000,
-                              radius: 100.0,
-                              lineWidth: 50.0,
-                              percent: folder.folderPercent * 0.01,
-                              center: Text(
-                                '${folder.folderPercent}%',
-                                style: const TextStyle(
-                                  fontSize: 32.0,
-                                  color: Colors.white,
+                                const Gap(100),
+                                CircularPercentIndicator(
+                                  animation: true,
+                                  animationDuration: 3000,
+                                  radius: 100.0,
+                                  lineWidth: 50.0,
+                                  percent: folder.folderPercent * 0.01,
+                                  center: Text(
+                                    '${folder.folderPercent}%',
+                                    style: const TextStyle(
+                                      fontSize: 32.0,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                  progressColor: Colors.blue,
                                 ),
-                              ),
-                              progressColor: Colors.blue,
-                            ),
-                          ]);
-                        },
-                      );
-                    }).toList(),
+                              ]);
+                            },
+                          );
+                        }).toList(),
+                      ),
+                    ),
                   ),
-                ),
-              ),
-      ),
-    );
+          ),
+        ));
   }
 }
