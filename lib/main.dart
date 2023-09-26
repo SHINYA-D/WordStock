f5638dc5d55237bd8fc3f7e91f5b88db062dfcf6
SHINYA <ttiuweqpp2901@gmail.com>
2023-09-27 08:07:41 +0900

    mainコメント削除

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:wordstock/app.dart';
 
-//mainにコメントを追加
 main() => runApp(const ProviderScope(child: App()));
